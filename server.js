a7a655a6bf1e59ff3f7838a29d437e15b9419b14
shultza <shultza@matc.edu>
2026-09-08 13:46:48 -0500

    Added database connection

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import connectDatabase from './config/db.js';
 const app = express();
+connectDatabase();
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
