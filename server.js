01839fae3cf512053bca29d671a837d4701b565d
shultza <shultza@matc.edu>
2026-09-15 13:27:18 -0500

    Tested API with Postman

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ import express from 'express';
 import connectDatabase from './config/db.js';
 const app = express();
 connectDatabase();
+//
+app.use(express.json({ extended: false}));
+//
 app.get('/', (req, res) => 
     res.send('http get request sent to root api endpoint')
 );
+//
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+//
 app.listen(3000, () => console.log(`Express server running on port 3000`));
