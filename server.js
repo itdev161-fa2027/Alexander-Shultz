5a6abdac3b21b43f8f701f0db8a0fa81509053d7
shultza <shultza@matc.edu>
2026-09-15 13:32:01 -0500

    Built data validation

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
+import { check, validationResult} from 'express-validator';
 const app = express();
 connectDatabase();
 //
@@ -9,9 +10,19 @@ app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 //
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+app.post('/api/users', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6+ characters').isLength({ min: 6})
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            //database code soon
+            return res.send(req.body);
+        }
+    }
+);
 //
 app.listen(3000, () => console.log(`Express server running on port 3000`));
